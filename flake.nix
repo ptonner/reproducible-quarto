5344c504486d5b4772ad68493069b6cc507ea6c6
Peter Tonner <peter.tonner@proton.me>
2024-09-29 10:52:47 -0400

    support custom kernels in quarto

--- flake.nix
+++ flake.nix
@@ -13,10 +13,54 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        # jupyter
+        jupyter = pkgs.jupyter.override {
+          definitions = {
+            clojure = pkgs.clojupyter.definition;
+            python_torch =
+              let
+                # python_torch = (builtins.head pkgs.python_torch.nativeBuildInputs);
+                python_torch = pkgs.python3.withPackages (ps: with ps; [ ]);
+              in
+              {
+                displayName = "PyTorch";
+                language = "python";
+                logo32 = "${pkgs.jupyter.sitePackages}/ipykernel/resources/logo-32x32.png";
+                logo64 = "${pkgs.jupyter.sitePackages}/ipykernel/resources/logo-64x64.png";
+                argv = [
+                  "${python_torch}/bin/python"
+                  "-m"
+                  "ipykernel_launcher"
+                  "-f"
+                  "{connection_file}"
+                ];
+              };
+          };
+        };
+        # quarto
+        # quarto = pkgs.quarto.override { python3 = jupyter; };
+        quarto = (pkgs.quarto.override { python3 = null; }).overrideAttrs (
+          final: prev: {
+            preFixup =
+              with builtins;
+              let
+                pfix = prev.preFixup;
+                plen = (stringLength pfix);
+                pfix_sub = substring 0 (plen - 1) pfix;
+              in
+              # (substring 0  prev.preFixup)
+              # prev.preFixup + "--prefix QUARTO_PYTHON : ${jupyter}/bin/python3";
+              pfix_sub + "--prefix QUARTO_PYTHON : ${jupyter}/bin/python3";
+          }
+        );
+        # blog
         blog = pkgs.stdenv.mkDerivation {
           name = "blog";
           src = ./blog;
-          buildInputs = [ pkgs.quarto ];
+          buildInputs = [
+            pkgs.quarto
+            jupyter
+          ];
           buildPhase = ''
             # Deno needs to add stuff to $HOME/.cache
             # so we give it a home to do this
@@ -32,9 +76,14 @@
         };
       in
       {
-        devShells.default = pkgs.mkShell { buildInputs = with pkgs; [ quarto ]; };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            quarto
+            jupyter
+          ];
+        };
         packages = rec {
-          inherit blog;
+          inherit blog quarto;
           default = blog;
         };
       }
